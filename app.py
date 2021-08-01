5ba595034efd6a6fd9146f429d440e859363d29c
unknown <iden.ticlla@gmail.com>
2021-07-31 20:13:07 -0400

    add documentation

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+from sys import version
 from fastapi import FastAPI
+from sqlalchemy.sql.expression import desc
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="my first API",
+    description="My primera API con fast",
+    version="0.0.1",
+    openapi_tags=[{
+        "name": "users",
+        "description": "users routes"
+    }]
+)
 app.include_router(user)
 
